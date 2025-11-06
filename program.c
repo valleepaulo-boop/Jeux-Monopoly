4b9de5ac4346d7cb2f24400df2363df8b7597c91
valleepaulo-boop <valleepaulo@gmail.com>
2025-11-06 22:53:39 +0000

    Graphique

--- program.c
+++ program.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello, World!\n");
+void plateau1(int){
+printf("________________________________________________________________________________________________________________________________\n");  
+printf("|    Départ     |     Fokker    |     Comac     |     Irkut     |               |   Bombardier  |   Bombardier  |    Embraer    |\n");        
+printf("|               |               |               |               |   PENALITE !  |               |               |               |\n");        
+printf("|     \\_        |      F100     |      C919     |     MC-21     |               |     Dash 8    |     CRJ-700   |     Kc-390    |\n");        
+printf("| \\____\\____    |               |               |               |Votre compagnie|               |               |               |\n");        
+printf("|      /_       |               |               |               |est black listé|               |               |               |\n");        
+printf("|     /         |               |               |               |               |               |               |               |\n");        
+printf("|               |               |               |               |   Vous devez  |               |               |               |\n");        
+printf("| Revenu mensuel|               |               |               | passe un tour |               |               |               |\n");        
+printf("|  vous gagné:  |    prix:50    |   prix:60     |    prix:70    |      OU       |   prix:100    |    prix:120   |   prix:140    |\n");        
+printf("|      200      |               |               |               |    Payez 25   |               |               |               |\n");        
+printf("|_______________|_______________|_______________|_______________|_______________|_______________|_______________|_______________|\n");
+}void plateau2(int){
+printf("________________________________________________________________________________________________________________________________\n");  
+printf("|    Audit      |    Embraer    |    Boeing     |    Boeing     |               |     Airbus    |     Airbus    |  Sud-Aviation |\n");        
+printf("|               |               |               |               |    BONUS !    |               |               |               |\n");        
+printf("|    Danger     |     E-190     |     737-8     |     777-3     |               |      A350     |      A380     |    Concorde   |\n");        
+printf("|      _        |               |               |               |Votre compagnie|               |               |               |\n");        
+printf("|     / \\       |               |               |               | est écologique|               |               |               |\n");        
+printf("|    /___\\      |               |               |               |               |               |               |               |\n");        
+printf("|               |               |               |               |   Vous allez  |               |               |               |\n");        
+printf("| A cause de la |               |               |               |    empocher:  |               |               |               |\n");        
+printf("|  maintenance, |   prix:160    |   prix:200    |    prix:220   |      250      |   prix:250    |    prix:300   |   prix:400    |\n");        
+printf("|   payez:200   |               |               |               |               |               |               |               |\n");        
+printf("|_______________|_______________|_______________|_______________|_______________|_______________|_______________|_______________|\n");
+}
+int main(){ 
+plateau1(1);
+plateau2(2);
+
     return 0;
 }
+
+
+
+
+
