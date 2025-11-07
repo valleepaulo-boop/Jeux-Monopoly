0ebd1826fd0cbcaf4f4279c2a971b07756e9b1ac
valleepaulo-boop <valleepaulo@gmail.com>
2025-11-07 09:30:37 +0000

    Partie 1 modif

--- program.c
+++ program.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 void plateau1(int){
 printf("________________________________________________________________________________________________________________________________\n");  
@@ -28,10 +31,40 @@ printf("| A cause de la |               |               |               |    emp
 printf("|  maintenance, |   prix:160    |   prix:200    |    prix:220   |      250      |   prix:250    |    prix:300   |   prix:400    |\n");        
 printf("|   payez:200   |               |               |               |               |               |               |               |\n");        
 printf("|_______________|_______________|_______________|_______________|_______________|_______________|_______________|_______________|\n");
+
+void pos1(int position){
+    position
+}
+void pos2(){
+    
 }
 int main(){ 
+int joueur1p=1000
+int joueur2p=1000
+printf("Bienvenue au monopoly, veuillez rentrer votre nom\n");
+printf("Joueur 1, quel est votre nom : \n");
+char joueur1[100];
+fgets(joueur1, 100, stdin);
+
+printf("Joueur 2, quel est votre nom :\n");
+char joueur2[100];
+fgets(joueur2, 100, stdin);
+
+srand(time(NULL)); // 1 seule fois en début de programme
+    
+int dice1;
+
+dice1 = rand()%6 + 1;
+printf("Résultat du lancé de dé %d \n", dice1);
+
 plateau1(1);
 plateau2(2);
+
+while (joueur1 || joueur2 ==0 || <0)
+{
+    position
+}
+
 
     return 0;
 }
